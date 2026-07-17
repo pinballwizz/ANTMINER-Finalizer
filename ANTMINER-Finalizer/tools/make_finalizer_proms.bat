8fba593c2fa7ce2b0548ed826870fcf9a8bf6e20
Pinball Wiz <70185819+pinballwizz@users.noreply.github.com>
2026-07-17 10:34:14 +1000

    Add files via upload

---
+++ ANTMINER-Finalizer/tools/make_finalizer_proms.bat
@@ -0,0 +1,22 @@
+make_vhdl_prom 523k01.9c eprom_1.vhd
+make_vhdl_prom 523k02.12c eprom_2.vhd
+make_vhdl_prom 523k03.13c eprom_3.vhd
+
+make_vhdl_prom 523h04.5e eprom_4.vhd
+make_vhdl_prom 523h07.5f eprom_5.vhd
+make_vhdl_prom 523h05.6e eprom_6.vhd
+make_vhdl_prom 523h08.6f eprom_7.vhd
+make_vhdl_prom 523h06.7e eprom_8.vhd
+make_vhdl_prom 523h09.7f eprom_9.vhd
+
+make_vhdl_prom 523h13.11f prom_1.vhd
+make_vhdl_prom 523h12.10f prom_2.vhd
+make_vhdl_prom 523h10.2f prom_3.vhd
+make_vhdl_prom 523h11.3f prom_4.vhd
+
+
+make_vhdl_prom snd01_715-057p.8a kSND01_ROM.vhd
+
+pause
+
+
